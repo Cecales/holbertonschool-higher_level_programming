fc21f33eafd12ee86592d5f01b4f548cd64c2d26
Cecales <cesar.caleroes@gmail.com>
2022-01-18 18:06:08 -0500

    First commit

---
+++ 0x06-ptyhon-classes/0-square.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+class Square:
+    """An object called Square"""
+    pass
---
+++ 0x06-ptyhon-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Class: Square
+    Attributtes: Size
+    """
+    def __init__(self, size):
+        self.__size = size
