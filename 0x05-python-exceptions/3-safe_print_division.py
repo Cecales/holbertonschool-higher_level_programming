03416d1f11f6079a7fd49ac6379f9991099f8501
Cecales <cesar.caleroes@gmail.com>
2022-01-17 23:44:49 -0500

    First Extra

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 def safe_print_division(a, b):
+    result = 0
     try:
         result = a / b
     except (ZeroDivisionError, FloatingPointError):
         result = None
     finally:
         print("Inside result: {:d}".format(result))
-    return result
+        return result
