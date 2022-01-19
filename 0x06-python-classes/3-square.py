7c3bfad53c038277627f5af215bd2292bd805c19
Cecales <cesar.caleroes@gmail.com>
2022-01-18 23:13:05 -0500

    Test more points

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Definition of the class Square"""
+
+
+class Square:
+    """An object called Square"""
+    def __init__(self, size=0):
+        """Initialize the size data"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Initialize the area data"""
+        return (self.__size * self.__size)
