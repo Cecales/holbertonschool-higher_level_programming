ab91c8ac51ecaeadfc8241c21ef4890b1cb66ab7
Cecales <cesar.caleroes@gmail.com>
2022-02-08 14:37:46 -0500

    corrections

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-"""
-models/base.py
-"""
+"""Module for the Base class"""
 
 
 import json
 
 
 class Base:
-    """
-
-    """
+    """Base class"""
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """
-
-        """
-
+        """Conductor for Base Class"""
         if type(id) != int and id is not None:
             raise TypeError("id must be an integer")
         if id is not None:
@@ -29,14 +22,14 @@ class Base:
 
     @staticmethod
     def to_json_string(l):
-        """"""
+        """Convets list to json string"""
         if l is None or len(l) == 0:
             return "[]"
         return json.dumps(l)
 
     @classmethod
     def save_to_file(cls, l):
-        """"""
+        """Save list of objects to json file"""
         with open(cls.__name__ + ".json", "w", encoding="utf-8") as f:
             if l is None:
                 f.write(Base.to_json_string([]))
@@ -48,7 +41,7 @@ class Base:
 
     @staticmethod
     def from_json_string(json_s):
-        """"""
+        """Converts json string to python object"""
         if json_s is None or not json_s:
             return []
         else:
