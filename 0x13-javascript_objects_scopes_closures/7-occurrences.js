86aedcb263e93c1afde5210d121ce46cf2ca93eb
Cecales <cesar.caleroes@gmail.com>
2022-04-05 20:14:26 -0500

    Test points

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,2 +1,12 @@
 #!/usr/bin/node
 
+exports.nbOccurences = function (list, searchElement) {
+  let n = 0;
+  list.forEach(count);
+  function count (item) {
+    if (item === searchElement) {
+      n++;
+    }
+  }
+  return n;
+};
