16408fe47dfbf7af3d1557c24af4b78311d69042
Cecales <cesar.caleroes@gmail.com>
2022-05-19 11:11:31 -0500

    O_o

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This is a Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -sX "$1" GET -H "X-School-User-Id: 98"
+curl -sH "X-School-User-Id:98" "$1"
